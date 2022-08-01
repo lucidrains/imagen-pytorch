fix a bug with order of normalizing and noising of the low resolution conditioning image in non-elucidated ddpm, thanks to @animebing !

imagen_pytorch/imagen_pytorch.py
@@ -2001,7 +2001,6 @@ class Imagen(nn.Module):
 
         batch = shape[0]
         img = torch.randn(shape, device = device)
-        lowres_cond_img = maybe(self.normalize_img)(lowres_cond_img)
 
         # prepare inpainting
 
@@ -2127,6 +2126,8 @@ class Imagen(nn.Module):
                     lowres_noise_times = self.lowres_noise_schedule.get_times(batch_size, lowres_sample_noise_level, device = device)
 
                     lowres_cond_img = resize_image_to(img, image_size)
+
+                    lowres_cond_img = self.normalize_img(lowres_cond_img)
                     lowres_cond_img, _ = self.lowres_noise_schedule.q_sample(x_start = lowres_cond_img, t = lowres_noise_times, noise = torch.randn_like(lowres_cond_img))
 
                 shape = (batch_size, self.channels, image_size, image_size)