c3afdf71dfbc0b7599e9ad5133c2af7046f672c8
Eddie Zhang <54998055+ezhang7423@users.noreply.github.com>
2022-10-04 17:06:33 -0700

    Bugfix in image size assertion (#230)

--- imagen_pytorch/elucidated_imagen.py
+++ imagen_pytorch/elucidated_imagen.py
@@ -167,8 +167,8 @@ class ElucidatedImagen(nn.Module):
 
         # unet image sizes
 
-        self.image_sizes = cast_tuple(image_sizes)
-        assert num_unets == len(image_sizes), f'you did not supply the correct number of u-nets ({len(self.unets)}) for resolutions {image_sizes}'
+        self.image_sizes = cast_tuple(self.image_sizes)
+        assert num_unets == len(self.image_sizes), f'you did not supply the correct number of u-nets ({len(self.unets)}) for resolutions {self.image_sizes}'
 
         self.sample_channels = cast_tuple(self.channels, num_unets)
 
